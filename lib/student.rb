6e697d0f7b7ec0e33c5feaaebe2db7abc655d761
Raul Sposito <raulsposito@gmail.com>
2020-07-07 22:46:10 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -88,7 +88,7 @@ class Student
       self.new_from_db(row)
     end
 
-    def self.first_X_students_in_grade_10
+    def self.first_X_students_in_grade_10(number)
       sql = <<-SQL
         SELECT *
         FROM students
