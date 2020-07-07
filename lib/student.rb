94e3706fe0e851ec13dbeffc23833b2f73f10031
Raul Sposito <raulsposito@gmail.com>
2020-07-07 22:55:32 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -102,4 +102,17 @@ class Student
     end
   end
 
+  def first_student_in_grade_10
+    sql = <<-SQL 
+      SELECT * 
+      FROM students 
+      WHERE grade = 10 
+      ORDER BY students.id LIMIT 1
+    SQL 
+
+    DB[:conn].execute(sql, grade).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
 end
