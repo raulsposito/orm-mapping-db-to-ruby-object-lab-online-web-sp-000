e8e1e2ddb6485a024979d68e1c421e32b300ab08
Raul Sposito <raulsposito@gmail.com>
2020-07-07 22:31:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -33,7 +33,9 @@ class Student
       LIMIT 1
     SQL
 
-    DB[:conn].execute(sql, self.name, self.grade)
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first 
   end
 
   def save
